a35b6343a190e706318388e3f5402276acbf13a3
Wasif Khan <waasif.khaan@gmail.com>
2018-07-17 21:08:08 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -56,20 +56,20 @@ class MusicLibraryController
       puts "#{i+1}. #{genre.name}"
     end 
   end
-  # def list_songs_by_artist
-  #   puts "Please enter the name of an artist:"
-  #   artist_name = gets.strip
-  #   #binding.pry
-  #   _artist = Artist.all.detect do|artist|
-  #       artist.name == artist_name
-  #     end 
-  #     if _artist
-  #       _artist.songs.sort {|a,b| a.name <=> b.name}.each_with_index do |song,i|
-  #           puts "#{i+1}. #{song.name} - #{song.genre.name}"
-  #       end 
-  #     else 
-  #     end
-  # end
+  def list_songs_by_artist
+    puts "Please enter the name of an artist:"
+    artist_name = gets.strip
+    #binding.pry
+    _artist = Artist.all.detect do|artist|
+        artist.name == artist_name
+      end 
+      if _artist
+        _artist.songs.sort {|a,b| a.name <=> b.name}.each_with_index do |song,i|
+            puts "#{i+1}. #{song.name} - #{song.genre.name}"
+        end 
+      else 
+      end
+  end
   # def list_songs_by_genre
   #   puts "Please enter the name of a genre:"
   #   genre_name = gets.strip
