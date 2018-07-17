bcf998da8f84364c26f2cb488ab7008ec4d4689d
Wasif Khan <waasif.khaan@gmail.com>
2018-07-17 20:32:08 +0000

    automatically backed up by learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -37,7 +37,7 @@ class Song
     @@all.detect {|song| song.name == song_name}
   end 
   def self.find_or_create_by_name(song_name)
-    self.find_by_name ? self.find_by_name : 
+    self.find_by_name ? self.find_by_name(song_name) : self.create(song_name) 
     end 
   end 
   
