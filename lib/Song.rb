f6a5740157d993fbfbba0a01b7dfd21066056fa0
Wasif Khan <waasif.khaan@gmail.com>
2018-07-17 20:53:16 +0000

    automatically backed up by learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -35,7 +35,14 @@ class Song
     @genre.songs << self  unless @genre.songs.include?(self)
   end 
   
-  def self.new_from_filename
+  def self.new_from_filename(filename)
+    song = Song.new(filename.split(" - ")[1])
+    artist = Artist.find_or_create_by_name(filename.split(" - ")[0])
+    genre = Genre.find_or_create_by_name(filename.split(" - ")[2].gsub(".mp3",""))
+    song.artist = artist
+    song.genre = genre
+    song
+  end 
     
   # def self.find_by_name(song_name)
   #   @@all.detect {|song| song.name == song_name}
